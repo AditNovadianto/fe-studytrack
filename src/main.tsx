c361070f1f314b12b154c19c3f8a03fda8ba09a4
AditNovadianto <aditya.novadianto@gmail.com>
2026-03-05 17:45:16 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  // {
+  //   path: '/signUp',
+  //   element: <SignUp />
+  // },
+  // {
+  //   path: '/forgot-password',
+  //   element: <ForgotPassword />
+  // },
+  // {
+  //   path: '/signInCustomer',
+  //   element: <SignInCustomer />
+  // },
+  // {
+  //   element: <ProtectedRoute />,
+  //   children: [
+  //     {
+  //       path: "/dashboard",
+  //       element: <Dashboard />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: '*',
+  //   element: <NotFound />
+  // },
+], {
+  future: {
+    v7_startTransition: true,
+    v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  } as any,
+}
+)
+
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
